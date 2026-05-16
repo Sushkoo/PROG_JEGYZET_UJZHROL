f879465d56f2a1d29950c6111978e1de0e4379c0
Sushi <70635877+Sushkoo@users.noreply.github.com>
2026-05-16 16:00:42 +0200

    Add files via upload

---
+++ Jegyzet.cpp
@@ -0,0 +1,327 @@
+#include <iostream>
+#include <string>
+#include <fstream> //fajlkezeles
+#include <cstdlib> // rand() fuggvenyhez kellene de lehet anelkul is jo 
+#include <vector> //dinamikus lista
+#include <stdexcept> //kivetelkezeles
+#include <sstream> //sor darabolas (,)
+
+using namespace std;
+
+class Product {
+private:
+    string kategoria;
+    string termeknev;
+    unsigned int lejarat; //unsigned: csak 0 vagy pozitiv szamok, nincs elojel
+    float ar;
+
+public:
+    //parameteres konstruktor
+    Product(const string& kategoria, const string& termeknev, unsigned int lejarat, float ar)
+        : kategoria(kategoria), termeknev(termeknev), lejarat(lejarat), ar(ar)
+    {
+    }
+    //alapertelmezett konstruktor
+    Product() = default;
+
+    //destruktor
+    ~Product() = default;
+
+    void akcio(int datum) {
+
+        if ((lejarat - datum) < 3)
+            ar /= (1 - (rand() / RAND_MAX) * 0.1 + 0.05);
+
+        cout << "Uj akcios ar: " << ar << endl;
+    }
+
+    //ezzel lehet fajlba és console-ra irni
+    virtual void kiir(ostream& kimenet = cout) const {
+        kimenet << kategoria << "," << termeknev << "," << lejarat << "," << ar;
+    }
+
+
+
+    //LEKERDEZO METODUSOK (GETTER)
+
+    string getKategoria() const { return kategoria; }
+
+    string getTermeknev() const { return termeknev; }
+
+    int getLejarat() const { return lejarat; }
+
+    float getAr() const { return ar; }
+
+    //MODOSITO METODUSOK (SETTER)
+
+    void setAr(float ujAr) {
+        if (ujAr >= 0) {
+            ar = ujAr;
+        }
+        else {
+            cout << "Hiba: Az ar nem lehet negatív!" << endl;
+        }
+    }
+    
+    void setKategoria(string ujKategoria) {
+        this->kategoria = ujKategoria; //shortcut
+    }
+
+    void setLejarat(unsigned int ujLejarat) {
+        this->lejarat = ujLejarat;
+    }
+
+    void setTermeknev(string ujTermeknev) {
+        this->termeknev = ujTermeknev;
+    }
+
+    //binaris kiiratashoz kell xd
+    void binariskiir(ofstream& bfajl) const {
+        // Bináris fájlba írásnál a stringek méretét is el kell menteni, különben nem tudjuk visszaolvasni!
+        size_t katMeret = kategoria.size();
+        bfajl.write(reinterpret_cast<const char*>(&katMeret), sizeof(katMeret));
+        bfajl.write(kategoria.c_str(), katMeret);
+
+        size_t nevMeret = termeknev.size();
+        bfajl.write(reinterpret_cast<const char*>(&nevMeret), sizeof(nevMeret));
+        bfajl.write(termeknev.c_str(), nevMeret);
+
+        // A számokat egyszerûen memóriacím alapján írjuk ki
+        bfajl.write(reinterpret_cast<const char*>(&lejarat), sizeof(lejarat));
+        bfajl.write(reinterpret_cast<const char*>(&ar), sizeof(ar));
+    }
+
+    
+};
+
+class Zoldseg : public Product {
+private:
+    unsigned int darabszam;
+
+public:
+    Zoldseg(string kategoria, string termeknev, unsigned int lejarat, float ar, unsigned int darabszam) : Product(kategoria,termeknev, lejarat, ar) //orokoltetes : utan
+    {
+        this->darabszam = darabszam; //setter shortcut
+    }
+
+    //getter a kiiratashoz kell
+    unsigned int getDarabszam() const { return darabszam; }
+
+    void kiir() { 
+        Product::kiir();
+        cout << "| Darabszam: " << darabszam << " db" << endl;
+    }
+
+};
+
+
+class Tejtermek : public Product {
+private:
+    unsigned int darabszam;
+
+public:
+    Tejtermek(string kategoria,string termeknev, unsigned int lejarat, float ar, unsigned int darabszam) : Product(kategoria,termeknev, lejarat, ar) {
+        this->darabszam = darabszam;
+    }
+
+    void kiir() {
+        Product::kiir();
+        cout << "| Darabszam: " << darabszam << " db" << endl;
+    }
+
+    unsigned int getDarabszam() const { return darabszam; }
+};
+
+int main()
+{
+    //statikus példányosítás (stack memory)
+    Product termek1("elelmiszer","Parizsi", 20260516,500);
+    Product termek2;
+
+
+    //getteres kiiratas
+    cout << "Kategoria: " << termek1.getKategoria() << endl;
+    //setter hasznalat
+    termek1.setAr(900.0);
+
+    //Dinamikus példányosítás (manualis memory)
+    Product* p1 = new Product("elelmiszer", "Parizsi", 10, 1500.5);
+    Product* p2 = new Product();
+
+    //Dinamikusat torolni kell a memory leak miatt
+    delete p1;
+    delete p2;
+
+
+
+
+    //amit a feladat elvar (bemutatni hogy mukodik minden):
+
+    cout << "------------------" << endl;
+
+    Product termekPelda;
+    termekPelda.setKategoria("Elelmiszer");
+    termekPelda.setTermeknev("Parizsi");
+    termekPelda.setAr(50000);
+    termekPelda.setLejarat(20260516);
+
+    cout << termekPelda.getKategoria() << endl;
+    cout << termekPelda.getAr() << endl;
+    cout << termekPelda.getLejarat() << endl;
+
+    Zoldseg repa("Zoldseg","repa", 20260516, 400, 20);
+
+    cout << "------------------" << endl;
+
+    termekPelda.kiir();
+    termekPelda.akcio(20260514);
+
+    //kiiratas txt-be
+    /*
+    ofstream fajl("termek_kimenet.txt"); //letrehoz/megnyitja
+
+    if (fajl.is_open())
+    {
+        fajl << "=== TERMEKEK ADATAI ===" << endl;
+
+        fajl << "Kategoria: " << repa.getKategoria()
+            << "| Lejarat: " << repa.getLejarat()
+            << "| Ar: " << repa.getAr() << "Ft"
+            << "| Darabszam: " << repa.getDarabszam() << "db" << endl;
+    
+
+    fajl.close();
+
+    cout << "\nSikeresen letrehoztam a 'termekek_kimenet.txt' fajlt" << endl;
+
+    }
+    else {
+        cout << "\nHiba: Nem sikerult letrehozni a fajlt" << endl;
+    }
+    */
+
+
+
+    //2. feladat allomanykezelo
+
+    //beolvaso
+
+    vector<Product> termekekLista;
+    string bemenetiFajlnev = "raktar.txt";
+
+    cout << "----- ALLOMANYKEZELO ALKALMAZAS -----" << endl;
+
+    //beolvasas hibakezelessel
+
+    try {
+        ifstream beolvaso(bemenetiFajlnev);
+
+        if (!beolvaso.is_open()) {
+            throw runtime_error("Hiba: Nem talalhato a bemeneti allomany");
+        }
+
+        string sor;
+        while (getline(beolvaso, sor)) {
+            stringstream ss(sor);
+            string beKategoria, beTermeknev, beLejaratStr, beArStr;
+
+        //sor darabolasa vesszovel
+            getline(ss, beKategoria, ',');
+            getline(ss, beTermeknev, ',');
+            getline(ss, beLejaratStr, ',');
+            getline(ss, beArStr, ',');
+
+
+            //szoveget-szamma (stoi: string-to-int, stof: string to float) 
+            unsigned int beLejarat = stoi(beLejaratStr);
+            float beAr = stof(beArStr);
+
+            //object letrehozasa es vektorba rakas
+            Product ujTermek(beKategoria, beTermeknev, beLejarat, beAr);
+            termekekLista.push_back(ujTermek);
+        }
+        beolvaso.close();
+        cout << "Sikeresen beolvasta" << termekekLista.size() << "db termek van a listaban" << endl;
+    }
+    catch (const exception& e) {
+        cerr << "Kivetel tortent: " << e.what() << endl;
+        return 1;
+    }
+
+
+
+    // ------2. INDEX / KATEGORIA SZERINTI VALASZTAS
+
+    vector<Product> kivalasztottTermekek;
+    string valasztas;
+    cout << "\nSzeretnel szurni a termekekre? (index / kategoria / nem): ";
+    cin >> valasztas;
+
+    try {
+        if (valasztas == "index") {
+            int index;
+            cout << "Add meg a termek indexet (0-" << termekekLista.size() - 1 << "): ";
+            cin >> index;
+
+            //hibakezeles
+            if (index < 0 || index >= termekekLista.size()) {
+                throw out_of_range("Hiba: Érvénytelen index!");
+            }
+
+            //Hozzadjuk a kivalasztott listahoz
+            kivalasztottTermekek.push_back(termekekLista[index]);
+            cout << "Termek Kivalasztva: " << termekekLista[index].getTermeknev() << endl;
+        }
+        else if (valasztas == "kategoria") {
+            string keresettKat;
+            cout << "Add meg a kategoriat (pl.Tejtermek): ";
+            cin >> keresettKat;
+
+            //megnezzuk az osszes termeket es ha egyezik akkor elmentjuk
+            for (const auto& termek : termekekLista) {
+                if (termek.getKategoria() == keresettKat) {
+                    kivalasztottTermekek.push_back(termek);
+                }
+            }
+            cout << kivalasztottTermekek.size() << " db termek kivalasztva ebbol a kategoriabol." << endl;
+        }
+        else {
+            //VEGSO ELSE ha nem szõr megkapja az osszeset
+            kivalasztottTermekek = termekekLista;
+        }
+    }
+    catch (const exception& e) {
+        cerr << "Kivetel torent: " << e.what() << endl;
+        return 1;
+    }
+
+    // ----- 3 KIIRAS (SZOVEGES VAGY BINARISBA)
+
+    if (!kivalasztottTermekek.empty()) {
+        try {
+            string kimenetiFajlnev = "kivalasztott_" + bemenetiFajlnev;
+            string mod;
+            cout << "\nMilyen formatumba szeretned menteni? (txt / binaris): ";
+            cin >> mod;
+
+            if (mod == "binaris") {
+                //ios:binary flag a binarishoz
+                ofstream kimenet(kimenetiFajlnev + ".bin", ios::binary);
+                if (!kimenet.is_open()) throw runtime_error("Nem lehet megnyitni a kimeneti binaris fajlt");
+
+                for (const auto& termek : kivalasztottTermekek) {
+                    termek.binariskiir(kimenet);
+                }
+                kimenet.close();
+                cout << "Sikeresen mentettem a BINARIS formatumba a " << kimenetiFajlnev << ".bin fajlba" << endl;
+            }
+        }
+        catch (const exception& e) {
+            cerr << "Kivetel tortent: " << e.what() << endl;
+        }
+    }
+    else {
+        cout << "Nincs mit kiirni (ures a kivalasztott lista)." << endl;
+    }
+    return 0;
+}
